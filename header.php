89104e080fe18d28e698ef810a80ad20a59b500c
Ignacio <shoik.xtazy@gmail.com>
2020-04-27 18:15:12 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css"
+        href="<?= bloginfo('template_directory')?>/assets/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= bloginfo('template_directory')?>/assets/css/main.css">
+    <title>Moto and Rock - We are stars</title>
+</head>
+
+<body>
+    <header>
+        <div class="logoMotoandRock"></div>
+    </header>
